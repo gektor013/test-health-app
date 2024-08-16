cf48083973648d25b381f5d94205c52772676fb1
gektor <bheiko@salerow.software>
2024-08-16 15:50:37 +0300

    bb-profile: implement password manager screen

--- src/features/password-manager/index.tsx
+++ src/features/password-manager/index.tsx
@@ -1,8 +1,74 @@
+import { colors } from "@/constants"
+import { Button, TextInput } from "@/shared/components"
+import { useTranslations } from "@/shared/hooks"
 import React from "react"
-import { StyleSheet, View } from "react-native"
+import { useForm } from "react-hook-form"
+import { Pressable, StyleSheet, Text, View } from "react-native"
 
 export const PasswordManager = () => {
-  return <View></View>
+  const { t } = useTranslations()
+  const { control } = useForm()
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          label="Password"
+          control={control}
+          name="password"
+          type="password"
+          inputProps={{
+            placeholder: t("Enter your password"),
+            style: styles.input
+          }}
+        />
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{t("Forgot password?")}</Text>
+        </Pressable>
+        <TextInput
+          label="New password"
+          control={control}
+          name="password"
+          type="password"
+          inputProps={{
+            placeholder: t("Enter your password"),
+            style: styles.input
+          }}
+        />
+        <TextInput
+          label="Confirm password"
+          control={control}
+          name="password"
+          type="password"
+          inputProps={{
+            placeholder: t("Enter your password"),
+            style: styles.input
+          }}
+        />
+      </View>
+      <Button title="Change Password" style={styles.button} />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  inputContainer: {
+    gap: 16
+  },
+  input: {
+    flexGrow: 1,
+    fontSize: 14
+  },
+  pressable: {
+    alignItems: "flex-end"
+  },
+  text: {
+    color: colors.green
+  },
+  button: {
+    borderRadius: 12
+  }
+})
